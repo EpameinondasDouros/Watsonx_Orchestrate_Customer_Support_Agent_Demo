b003a2845b8658b2a9725cb9e2c80368af096c05
Epameinondas Douros <Epameinondas.Douros@ibm.com>
2025-11-04 11:46:06 +0200

    new file:   Email_VM/knowledge_base/Customer_Inquiry_Contacts.pdf 	new file:   Email_VM/knowledge_base/Customer_Response_Guidelines.pdf 	new file:   Email_VM/knowledge_base/answer_manual.yaml 	new file:   Email_VM/knowledge_base/inquiries_info.yaml 	modified:   Email_VM/setup_orchestrate_agent.sh

--- Email_VM/setup_orchestrate_agent.sh
+++ Email_VM/setup_orchestrate_agent.sh
@@ -15,13 +15,21 @@ echo "-------------------------------------------"
 echo "📦 Installing Python dependencies..."
 pip install -r requirements.txt
 
-# 2. Import custom tools
+# 2. Import custom tools and knowledge base
 echo "🧰 Importing Python tools into Orchestrate..."
 
-orchestrate tools import -k python -f tools/gmail_auto_sender_tool.py -r requirements.txt
-orchestrate tools import -k python -f tools/contracts_information_tool.py -r requirements.txt
-orchestrate tools import -k python -f tools/client_lookup_tool.py -r requirements.txt
-orchestrate tools import -k python -f tools/gmail_get_latest_email_tool.py -r requirements.txt
+orchestrate tools import -k python -f tools/gmail_auto_sender_tool.py -r requirements.txt --app-id google_oauth2_auth_code_ibm_184bdbd3
+orchestrate tools import -k python -f tools/contracts_information_tool.py -r requirements.txt --app-id google_oauth2_auth_code_ibm_184bdbd3
+orchestrate tools import -k python -f tools/client_lookup_tool.py -r requirements.txt --app-id google_oauth2_auth_code_ibm_184bdbd3
+orchestrate tools import -k python -f tools/gmail_get_latest_email_tool.py -r requirements.txt --app-id google_oauth2_auth_code_ibm_184bdbd3
+
+echo "Importing Knowledge Database"
+orchestrate knowledge-bases import -f knowledge_base/answer_manual.yaml
+orchestrate knowledge-bases import -f knowledge_base/inquiries_info.yaml
+
+echo "Checking Knowledge Base Status"
+orchestrate knowledge-bases status --name answer_manual
+orchestrate knowledge-bases status --name inquiries_info
 
 # 3. Import the agent YAML definition
 echo "🤖 Importing the email handler agent..."
